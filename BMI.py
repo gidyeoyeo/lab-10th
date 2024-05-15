6c13759b85818b5e4ac75b9279134721c19d1aad
Gideon <JWYEO.23@ichat.sp.edu.sg>
2024-05-15 22:06:42 +0800

    new BMI which return value like 0 1 -1

--- BMI.py
+++ BMI.py
@@ -7,8 +7,11 @@ def calculate_bmi(height, weight):# weight and height are now integer and float 
 
     if bmi<18.5:
         print("under weight")
+        return(-1)
     elif bmi<=25.0 and bmi>=18.5:
         print("Normal weight")
+        return(0)
     elif bmi>25.0:
         print("overweight")
+        return(1)
 calculate_bmi(weight=57, height=1.73)
